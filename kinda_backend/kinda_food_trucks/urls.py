94bc83855e7d74488d643c90ccfe74e9db999cc0
Nico Bellanich <nicolasbellanich@gmail.com>
2022-10-14 01:02:22 -0300

    Change Url description name

--- kinda_backend/kinda_food_trucks/urls.py
+++ kinda_backend/kinda_food_trucks/urls.py
@@ -7,8 +7,8 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("all/", views.get_all_food_trucks_from_database, name="all"),
+    path("all/", views.get_all_food_trucks_from_database, name="all_db"),
     path("<int:food_truck_id>/", views.get_one_food_truck_by_id_from_database, name="by_id"),
-    path("sf_food_trucks", views.get_sf_food_trucks, name="other"),
+    path("sf_food_trucks", views.get_sf_food_trucks, name="sf_trucks"),
     path("<int:st_number>/<str:st_name>", views.calculate_closer_food_truck, name="closer"),
 ]
